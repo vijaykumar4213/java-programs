8c36dc8e55398d704bfc3880513186cf2fd9aa0e
vijaykumar4213 <vijaykoppala4213@gmail.com>
2025-08-09 22:03:46 +0530

    Create fizzBuzz.java

---
+++ fizzBuzz.java
@@ -0,0 +1,23 @@
+
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	  
+	    Scanner sc=new Scanner(System.in);
+	   int n=sc.nextInt();
+	   
+	    if((n%3==0)&&(n%5==0))
+	    {
+	       System.out.println("fizzbuzz");
+	    }
+	    else if(n%3==0)
+	    {
+	      System.out.println("fizz");
+	    }
+	    else if(n%5==0)
+	    {
+		System.out.println("buzz");
+	}
+}
+}
