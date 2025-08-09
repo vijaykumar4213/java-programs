f68b0418930bf484600798debc26842bab0d3c7e
vijaykumar4213 <vijaykoppala4213@gmail.com>
2025-08-09 22:13:29 +0530

    Create Nim Game.java

---
+++ Nim Game.java
@@ -0,0 +1,20 @@
+
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	  
+	    Scanner sc=new Scanner(System.in);
+	   int n=sc.nextInt();
+	   
+	    if(n%4==0)
+	    {
+	       System.out.println("loss");
+	    }
+	    else 
+	    {
+	      System.out.println("win");
+	    }
+
+}
+}
