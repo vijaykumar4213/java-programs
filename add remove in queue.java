233f2f89bd6a766e521df8f33fcd0a753cee090a
vijaykumar4213 <vijaykoppala4213@gmail.com>
2025-08-09 22:01:03 +0530

    Add files via upload

---
+++ add remove in queue.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Queue<Integer> q=new LinkedList<>();
+	    q.add(1);
+	    q.add(2);
+	    System.out.println(q.remove());
+		System.out.println(q);
+	}
+}
