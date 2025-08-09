26e14a012a030279d944119d48a91059cf175e73
vijaykumar4213 <vijaykoppala4213@gmail.com>
2025-08-09 22:21:33 +0530

    Create linkedlist middle.java

---
+++ linkedlist middle.java
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode s=head;
+        ListNode f=head;
+        while((f.next!=null)&&(f.next.next!=null))
+        {
+            f=f.next.next;
+            s=s.next;
+        }
+        if(f.next==null)return s;
+        else return s.next;
+        
+    }
+}
